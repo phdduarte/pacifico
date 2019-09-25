f5c127500b6f829489b76d24229a3724bde64e27
Pedro Henrique Lage Duarte <duartep@google.com>
2019-09-25 19:37:32 -0300

    form 1.0

--- public/smtp.php
+++ public/smtp.php
@@ -1,38 +1,66 @@
 <?php
-if(!empty($_POST))
-{
-    $name = $_POST['name'];
-
-    /*/ this is the email we get from visitors*/
-    $domain_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-    $redirect_url = 'https://www.pacificocontabilidade.com.br/v1/img/mentiras.pdf';
-
-    /*//-->MUST BE 'https://';*/
-    header("Content-type: application/json");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Origin: *.ampproject.org");
-    header("AMP-Access-Control-Allow-Source-Origin: ".$domain_url);
+// Fixing CORS for AMP
+header("access-control-allow-credentials:true");
+header("access-control-allow-headers:Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token");
+header("access-control-allow-methods:POST, GET, OPTIONS");
+header("access-control-allow-origin:".$_SERVER['HTTP_ORIGIN']);
+header("access-control-expose-headers:AMP-Redirect-To,AMP-Access-Control-Allow-Source-Origin");
+header("AMP-Access-Control-Allow-Source-Origin:".$_SERVER['HTTP_ORIGIN']);
+header("Content-Type: application/json");
 
 
-    /*/ For Sending Error Use this code /*/
-    if(!mail("contato@grazyferreiraemagrecimento.com.br" , "Test submission" , "email: $name <br/> name: $name" , "From: $name\n ")){
-        header("HTTP/1.0 412 Precondition Failed", true, 412);
+$name = $_POST["nome"];
+$email = $_POST["email"];
+$tel = $_POST["tel"];
 
-        echo json_encode(array('errmsg'=>'There is some error while sending email!'));
-        die();
-    }
-    else
-    {
-        /*/--Assuming all validations are good here--*/
-        if( empty($redirect_url))
-        {
-            header("Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin");
-        }
-        else
-        {
-            header("AMP-Redirect-To: ".$redirect_url);
-            header("Access-Control-Expose-Headers: AMP-Redirect-To, AMP-Access-Control-Allow-Source-Origin");        }
-            echo json_encode(array('successmsg'=>$_POST['name'].'My success message. [It will be displayed shortly(!) if with redirect]'));
-        die();
-    }
-}?>
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'email-ssl.com.br';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'ffigueiredo@pacificocontabilidade.com.br';                     // SMTP username
+    $mail->Password   = 'k*z$Bb89';                               // SMTP password
+    //$mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('ffigueiredo@pacificocontabilidade.com.br', 'SITE');
+    $mail->addAddress('ffigueiredo@pacificocontabilidade.com.br', 'ffigueiredo@pacificocontabilidade.com.br');     // Add a recipient
+    //$mail->addAddress('contato@pdwebdesign.com.br');               // Name is optional
+    //$mail->addReplyTo('contato@pdwebdesign.com.br', 'Information');
+    //$mail->addCC('contato@pdwebdesign.com.br');
+    //$mail->addBCC('contato@pdwebdesign.com.br');
+
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Lead recebido do SITE - ' . $name;
+    $mail->Body    = 'NOME: ' . $name . '<br>' . 'E-MAIL: ' . $email . '<br>' . 'TELEFONE: ' . $tel;
+    //$mail->AltBody = 'TELEFONE:';
+
+    $mail->send();
+    $msg = [];
+    $msg['nome'] = $name;
+    
+    echo json_encode($msg);
+} catch (Exception $e) {
+    $msg = 'Message could not be sent. Mailer Error: {$mail->ErrorInfo}';
+    echo json_encode($msg);
+}
